c6cef1938fe29cf2833603b19fcec63e8493ba48
Tarek Belkahia <tarek.belkahia@gmail.com>
2018-10-06 13:20:16 +0200

    Implement JS interface

--- src/jsMain/kotlin/coinche/Main.kt
+++ src/jsMain/kotlin/coinche/Main.kt
@@ -1,41 +1,47 @@
 package coinche
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlin.browser.window
+
+fun main() {
+    GlobalScope.launch { runGame() }
+}
+
 actual fun inputCard(playableCards: Set<Card>): Card {
-    return playableCards.first()
+    val input = window.prompt("Select card to play: $playableCards")
+    return input!!.cards().first()
 }
 
 actual fun showGameState(state: GameState) {
     val (update, game) = state
     when (update) {
         Update.NEW_GAME -> {
-            println("Game starting.")
-            println()
+            window.alert("Game starting.")
         }
         Update.NEW_ROUND -> {
-            println("New round.")
-            game.currentRound.players.forEach { e -> println("${e.key} |${e.value.hand}") }
-            println("Bid: ${game.currentRound.bid}")
-            println()
+            val hands = game.currentRound.players.map { e -> "${e.key} |${e.value.hand}" }
+            window.alert("""
+                |New round.
+                |${hands.joinToString("\n")}
+                |Bid: ${game.currentRound.bid}""".trimMargin())
         }
-        Update.NEW_TRICK -> {
-        }
+        Update.NEW_TRICK -> { }
         Update.ADVANCE_TRICK -> {
-            println("${game.currentTrick}")
+            window.alert("${game.currentTrick}")
         }
         Update.END_TRICK -> {
-            println()
-            println("${game.currentRound.startingPosition} won the trick!")
-            println("${game.currentRound.tricks.size} tricks played.")
-            println("Points: ${game.currentRound.currentPoints}.")
-            println()
+            window.alert("""
+                |${game.currentRound.startingPosition} won the trick!
+                |${game.currentRound.tricks.size} tricks played.
+                |Points: ${game.currentRound.currentPoints}.""".trimMargin())
         }
         Update.END_ROUND -> {
-            println("Round done.")
-            println("Score is ${game.score}.")
-            println()
+            window.alert("""Round done.
+                            |Score is ${game.score}.""".trimMargin())
         }
         Update.END_GAME -> {
-            println("Game over.")
+            window.alert("Game over.")
         }
     }
 }
