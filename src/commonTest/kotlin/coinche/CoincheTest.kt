75c50d35d5f6b56cbb3d6472dc18c651f61e905f
Tarek Belkahia <tarek.belkahia@gmail.com>
2018-10-06 12:50:33 +0200

    Rename tests for js

--- src/commonTest/kotlin/coinche/CoincheTest.kt
+++ src/commonTest/kotlin/coinche/CoincheTest.kt
@@ -9,7 +9,7 @@ import kotlin.test.assertTrue
 class CoincheTest {
 
     @Test
-    fun `player can play any cards if he starts`() {
+    fun player_can_play_any_cards_if_he_starts() {
         val playerHand = "7♣ 8♣ 9♣ 10♣ J♣ 7♦".cards()
         val actualPlayableCards = findPlayableCards(
             trick = createTrick(
@@ -22,7 +22,7 @@ class CoincheTest {
     }
 
     @Test
-    fun `player must played regular asked color if he has one`() {
+    fun player_must_played_regular_asked_color_if_he_has_one() {
         val actualPlayableCards = findPlayableCards(
             trick = createTrick(
                 cards = "8♥ A♥".cards(),
@@ -34,7 +34,7 @@ class CoincheTest {
     }
 
     @Test
-    fun `player must cut`() {
+    fun player_must_cut() {
         val playerHand = "7♣ 8♣ 9♣ 10♣ J♣ 7♠".cards()
         val actualPlayableCards = findPlayableCards(
             trick = createTrick(
@@ -47,7 +47,7 @@ class CoincheTest {
     }
 
     @Test
-    fun `player can piss if partner is winning`() {
+    fun player_can_piss_if_partner_is_winning() {
         val playerHand = "7♣ 8♣ 9♣ 10♣ J♣ 7♠".cards()
         val actualPlayableCards = findPlayableCards(
             trick = createTrick(
@@ -60,7 +60,7 @@ class CoincheTest {
     }
 
     @Test
-    fun `player must play higher trump`() {
+    fun player_must_play_higher_trump() {
         val playerHand = "7♣ 8♣ 9♣ 10♣ J♣ 7♠ J♠".cards()
         val actualPlayableCards = findPlayableCards(
             trick = createTrick(
@@ -73,7 +73,7 @@ class CoincheTest {
     }
 
     @Test
-    fun `player must play higher trump if opponent cut`() {
+    fun player_must_play_higher_trump_if_opponent_cut() {
         val playerHand = "7♣ 8♣ 9♣ 10♣ J♣ 7♠ J♠".cards()
         val actualPlayableCards = findPlayableCards(
             trick = createTrick(
@@ -86,7 +86,7 @@ class CoincheTest {
     }
 
     @Test
-    fun `player can play any card if opponent cut with higher trump`() {
+    fun player_can_play_any_card_if_opponent_cut_with_higher_trump() {
         val playerHand = "7♣ 8♣ 9♣ 10♣ J♣ 7♠".cards()
         val actualPlayableCards = findPlayableCards(
             trick = createTrick(
@@ -99,7 +99,7 @@ class CoincheTest {
     }
 
     @Test
-    fun `player can play any cards if he has not asked suit nor trump`() {
+    fun player_can_play_any_cards_if_he_has_not_asked_suit_nor_trump() {
         val playerHand = "7♣ 8♣ 9♣ 10♣ J♣ 7♦".cards()
         val actualPlayableCards = findPlayableCards(
             trick = createTrick(
@@ -113,7 +113,7 @@ class CoincheTest {
 
 
     @Test
-    fun `partner is winning`() {
+    fun partner_is_winning() {
         val trick = createTrick(
             cards = "A♥ 8♥".cards(),
             currentPlayer = createPlayer()
@@ -122,7 +122,7 @@ class CoincheTest {
     }
 
     @Test
-    fun `partner is not winning`() {
+    fun partner_is_not_winning() {
         val trick = createTrick(
             cards = "8♥ A♥".cards(),
             currentPlayer = createPlayer()
